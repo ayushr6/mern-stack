e41c897f74f15ae30b88f5214e7377fa0f60a275
ayushr6 <ayushranwa6@gmail.com>
2022-10-23 14:38:02 +0530

    captcha validation complete

--- captcha-generator/script.js
+++ captcha-generator/script.js
@@ -24,6 +24,33 @@ const textGenerator = () => {
 // Generate random numbers between a gitven range
 const randomNumber = (min,max) => Math.floor(Math.random() * (max - min + 1) + min);
 
+//Canvas part
+function drawStringOnCanvas(string) {
+    // The getContext() function returns the drawing context that has all the drawing properties
+    // and functions needed to draw on canvas
+    let ctx = canvas.getContext("2d");
+    // clear canvas
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    // array of text color
+    const textColors = ["rgb(0,0,0)", "rgb(130,130,130)"];
+    // space between letters
+    const letterSpace = 150 / string.length;
+    // loop through string
+    for(let i=0;i<string.length;i++) {
+        // Define initial space on X axis
+        const xInitialSpace = 25;
+        // set text color
+        ctx.fillStyle = textColors[randomNumber(0,1)];
+        ctx.fillText(
+            string[i],
+            xInitialSpace + i*letterSpace,
+            randomNumber(25, 40),
+            100
+        );
+    }
+}
+
+// Initial Function
 const triggerFunction = () => {
     // clear Input
     userInput.value = "";
@@ -33,3 +60,20 @@ const triggerFunction = () => {
     text = [...text].sort(() => Math.random() - 0.5).join("");
     drawStringOnCanvas(text);
 }
+
+// call triggerFunction() for reload button
+reloadButton.addEventListener("click", triggerFunction);
+
+// call triggerFunction() when page loads
+window.onload = () => triggerFunction();
+
+// When user clicks on submit
+submitButton.addEventListener("click", () => {
+    // check if user input == generated text
+    if(userInput.value === text) {
+        alert("Success");
+    } else {
+        alert("Incorrect");
+        triggerFunction();
+    }
+});
